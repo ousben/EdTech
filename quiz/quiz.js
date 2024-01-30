283f513ffed75da927511426be0b792dfece0a6f
unknown <benguiratoussama1998@gmail.com>
2024-01-30 20:22:34 +0100

    changes

---
+++ quiz/quiz.js
@@ -0,0 +1,7 @@
+const questionWrapper = document.getElementsByTagName("question-wrapper");
+const submitButton = document.getElementById("submit-button");
+const main = document.
+
+submitButton.addEventListener("click", function() {
+  let
+});
