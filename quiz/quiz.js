6bbba18061faa16b71f6d6a3ccfdc3fa84c90fac
unknown <benguiratoussama1998@gmail.com>
2024-01-30 23:42:07 +0100

    changes

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,7 +1,25 @@
-const questionWrapper = document.getElementsByTagName("question-wrapper");
-const submitButton = document.getElementById("submit-button");
-const main = document.
+const questionWrapper = document.getElementsByClassName("question-wrapper");
+const submitButton = document.getElementsByClassName("submit-button");
+const main = document.getElementById("main");
+const toast = document.getElementById("toast");
+const toastEnd = document.getElementById("toast-end");
 
-submitButton.addEventListener("click", function() {
-  let
-});
+for(let i = 0; i < submitButton.length; i++) {
+  submitButton[i].addEventListener("click", function() {
+    if(i != 4) {
+      document.getElementById(`question-${i+1}`).classList.remove("active-question");
+      document.getElementById(`question-${i+2}`).classList.add("active-question");
+      
+      toast.showModal()
+      setTimeout(() => {
+        toast.close();
+      }, 2000);
+    } else {
+      toastEnd.showModal()
+      setTimeout(() => {
+        toast.close();
+        window.location.href = "/course/course.html";
+      },2000);
+    }
+  });
+}
